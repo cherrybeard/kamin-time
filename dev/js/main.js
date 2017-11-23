dba541a0e0ebf0b241e20340a0127fc4119cd028
Nalopka <Nalopka@gmail.com>
2017-11-23 16:57:42 +0700

    progress

---
+++ dev/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    var mySwiper = new Swiper ('.swiper-container', {
+      // direction: 'vertical',
+      loop: true,
+      // slidesPerView: 1
+      navigation: {
+   		nextEl: '.swiper-button-next',
+    	prevEl: '.swiper-button-prev'
+  		},
+    })
+  });
