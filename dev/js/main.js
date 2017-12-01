f7586e2cac5f765cc5a5c6a2e64df9333453cf7f
Nalopka <Nalopka@gmail.com>
2017-12-01 11:10:55 +0700

    add callback button
    callback button + js code

--- dev/js/main.js
+++ dev/js/main.js
@@ -35,6 +35,53 @@ if (this.hash !== "") {
     } // End if
   });
 
+// CALLBACK
+
+      var $callback = $("[data-callback-show]");
+      var $callbackForm = $("[data-callback-form]");
+
+      function checkInputs($inputs){
+        $inputs.each(function(){
+          var $input = $(this).closest('.text-input');
+          if (!$(this).val()) {$input.addClass('error'); }
+        });
+      }
+
+      $callback.click(function(){
+        $(this).closest('[data-callback-state]').attr('data-callback-state', 'form');
+      });
+
+      $callbackForm.on('focus', 'input', function(){
+        $(this).closest('.text-input').removeClass('error');
+      });
+
+      $callbackForm.on('click', '.text-input', function(){
+        $(this).find('input').focus();
+      });
+
+      $callbackForm.on('submit', function(e){
+        var $this = $(this);
+        e.preventDefault();
+
+        checkInputs($this.find('input[type=tel]'));
+
+        if (!$this.find('.text-input.error').length){
+          var ajaxData = {
+            type: $this.attr('method'),
+            data: $this.serialize()
+          };
+          $this.find('button[type=submit]').prop('disabled', true);
+          $this.find('.text-input').addClass('disabled');
+          $.ajax(ajaxData).done(function() {
+            $this.find('button[type=submit]').prop('disabled', false).addClass('success').text('Отправлено!');
+          });
+        }
+      });
+
+
+
+
+
 
 });
 
