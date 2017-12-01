086be9cd587531bd10baa0bc6968f3e0b5264613
Steven Gusev <stephen.gusev@gmail.com>
2017-12-01 21:48:12 +0700

    codekit setup

--- public/index.php
+++ public/index.php
@@ -1,3 +1,26 @@
+<?
+	if (count($_POST)!=0) {
+		$msg = 'Новая заявка с лендинга KaminTime #1<br>';
+		if (isset($_POST['phone'])) {
+			$msg .= 'Телефон: '.$_POST['phone'].'<br>';
+		}
+		if (isset($_POST['email'])) {
+			$msg .= 'E-mail: '.$_POST['email'].'<br>';
+		}
+		if (isset($_POST['message'])) {
+			$msg .= 'Хочу: '.$_POST['message'].'<br>';
+		}
+		$title = 'Заявка с лендинга KaminTime #1';
+		if (!empty($_POST['msg'])) $msg .= '<br>'.$_POST['msg'];
+    $to = 'shantishanti@bk.ru';
+		$header = 'MIME-Version: 1.0' . "\n" .
+			'Content-type: text/html; charset=UTF-8'. "\n" .
+			"From: KaminTime <shantishanti@bk.ru>\n";
+    if (@mail($to, $title, $msg, $header))
+    	die('1');
+	}
+	if (count($_POST)!=0) die('0');
+?>
 <!DOCTYPE html>
 <html lang="ru">
   <head>
