17e5219e400ccc0d2617174364d71dc31d708379
Nalopka <Nalopka@gmail.com>
2017-12-05 11:13:36 +0700

    footer fix

--- public/main.min.js
+++ public/main.min.js
@@ -1,8 +1,6 @@
 $(function() {
   var mySwiper = new Swiper ('.swiper-container', {
-    // direction: 'vertical',
     loop: true,
-    // slidesPerView: 1
     navigation: {
  		nextEl: '.swiper-button-next',
   	prevEl: '.swiper-button-prev'
